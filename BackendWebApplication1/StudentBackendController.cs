99f7dc74e3a8268fb9eb5cb599b6a720a0c2255e
Adam <akhrorkuloww@gmail.com>
2023-04-05 22:10:18 +0500

    Add project files.

---
+++ BackendWebApplication1/StudentBackendController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentBackendApplicationData;
+
+namespace BackendWebApplication1
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentBackendController : ControllerBase
+    {
+
+        private readonly ILogger<StudentBackendController> _logger;
+
+        public StudentBackendController(ILogger<StudentBackendController> logger)
+        {
+            _logger = logger;
+        }
+
+        //write a new get method that will get data from db
+        [HttpGet(Name = "GetStudentsData")]
+        public IEnumerable<Student> GetData()
+        {
+            var ds = new StudentDataService();
+            var res = ds.GetData();
+            return res;
+
+        }
+        
+    }
+}
