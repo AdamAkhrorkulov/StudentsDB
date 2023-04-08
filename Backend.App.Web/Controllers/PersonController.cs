a5a4c6092bc8d378f19ed690a610e079627973a9
Adam <akhrorkuloww@gmail.com>
2023-04-09 00:34:01 +0500

    added controlers

---
+++ Backend.App.Web/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Backend.App.Data.Sevices;
+using Backend.App.Models.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.App.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        //write a new get method that will get data from db
+        [HttpGet(Name = "GetDbData")]
+        public IEnumerable<Person> GetData()
+        {
+            PersonDataService ds = new PersonDataService();
+            var res = ds.GetData();
+            return res;
+
+        }
+        //create new studentController
+        //GetStudentData, will get data from db. 
+        //create StudentDataService = 
+        //create Student class in models project
+    }
+}
